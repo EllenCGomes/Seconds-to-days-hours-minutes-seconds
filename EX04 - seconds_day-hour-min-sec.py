d634a9745e8c5303b2f38a8b5a34808fc153e70e
Ellen Gomes <78104233+EllenCGomes@users.noreply.github.com>
2022-04-01 16:11:12 -0300

    Update EX04 - seconds_day-hour-min-sec.py

--- EX04 - seconds_day-hour-min-sec.py
+++ EX04 - seconds_day-hour-min-sec.py
@@ -6,8 +6,8 @@ days = int((float(seconds))//86400)
 rest1 = int((float(seconds))%86400)
 hours = int(rest1//3600)
 rest2 = int(rest1%3600)
-min = int(rest2//60)
+minute = int(rest2//60)
 seg = int(rest2%60)
 
-print("{} days, {} hours, {} minutes and {} seconds.".format(days, hours, min, seg))
+print(f"{days} days, {hours} hours, {minute} minutes and {}seg seconds.")
 
